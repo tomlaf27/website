0e97a52ae2b397b13f77dcdf3d2b1f34192ad6de
Tom <tomlafemina1@gmail.com>
2018-10-15 21:00:55 -0400

    my first real commit

---
+++ react-client/src/Components/HomePage.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+class HomePage extends Component {
+  render() {
+    return (
+      <div>
+        Home page
+        <h1>Hello Leeshy</h1>
+      </div>
+    )
+  }
+}
+export default HomePage;
